45a8959b25610daf8ad314eacba312cbc84b1088
Nancy Zaky <nancyzki@yahoo.com>
2021-06-28 16:48:21 -0700

    commit tree d2044b1b02ff675363671370341bac1c6fa65315

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (loc)=> {
+  const blocks = loc < 42? 42-loc : loc-42;
+  return blocks;
+}
+
+const distanceFromHqInFeet = (loc)=> {
+return distanceFromHqInBlocks(loc) * 264;
+}
+
+const distanceTravelledInFeet = (start, end) => {
+  const distance = start>end ? start-end : end-start;
+  return distance * 264
+
+}
+const calculatesFarePrice = (start, end)=> {
+let cost;
+const distanceInFeet = distanceTravelledInFeet(start, end);
+if (distanceInFeet <= 400) {return 0}
+ else if (distanceInFeet >= 2500) {
+   return 'cannot travel that far' ;
+ } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    cost = distanceInFeet - 400;
+    return cost * 2 /100;
+ } else if (distanceInFeet >2000 && distanceInFeet< 2500) {
+   return 25;
+ }
+}
+
